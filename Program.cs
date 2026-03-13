8d61c40132c787939e8479662061043fca075a15
AleMuñoz <lizet_munoz@cpm.coop>
2026-03-13 10:55:50 -0600

    test github actions

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/weatherforecast", () => Results.Json("docker alek using webhooks"));
+app.MapGet("/weatherforecast", () => Results.Json("docker ale using webhooks"));
 
 app.Run();
 // var builder = WebApplication.CreateBuilder(args);
